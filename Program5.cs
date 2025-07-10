fd109be3057fe2c7181124b118827a403e8df78e
Ayushi💕 <ayushithummar14@gmail.com>
2025-07-10 10:39:23 +0530

    updtaed Program5.cs

--- Program5.cs
+++ Program5.cs
@@ -10,7 +10,17 @@ namespace Ayushi_PJT
     {
         public static void Main(string[] args)
         {
-
+            // Printing tables from 1 to 10.
+            for(int i=1; i<=10; i++)
+            {
+                //Console.WriteLine("1 * "+ i+ " = "+1 * i);
+                for (int j = 1; j <= 10; j++)
+                {
+                    int k = i * j;
+                    Console.WriteLine(i+" * "+" = "+k);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
