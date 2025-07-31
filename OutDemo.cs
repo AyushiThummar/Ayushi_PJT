4d49596cb34aa89ea67aea2582da73817111a528
Ayushi💕 <ayushithummar14@gmail.com>
2025-07-31 10:14:17 +0530

    Change and Upadte

---
+++ OutDemo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Ayushi_PJT
+{
+    internal class OutDemo
+    {
+        static void Change(ref int a, ref int b)
+        {
+            a = 100;
+            //b = 200;
+
+        }
+        static void Update(out int x, out int y) // For returning multiple outputs
+        {
+            x = 100; //1st
+            int t = x;
+            y = 200; //2nd
+
+        }
+        static void Main()
+        {
+            int a = 10, b = 20;
+            int x = 10, y = 20;
+            Console.WriteLine("Before change : a = {0} and b = {1}", a, b);
+            Change(ref a, ref b);
+            Console.WriteLine("After change : a = {0} and b = {1}", a, b);
+
+            Console.WriteLine("Before Update : x = {0} and y = {1}", x, y);
+            Change(ref a, ref b);
+            Console.WriteLine("After Update : x = {0} and y = {1}", x, y);
+        }
+    }
+}
